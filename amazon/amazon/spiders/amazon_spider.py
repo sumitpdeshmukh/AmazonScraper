df4b4c5e80d5b878bfbe625098f9ca83ad0d03aa
SeenaThite <cinathite@gmail.com>
2016-08-09 20:51:48 +0530

    Added Category wise scraping code

--- amazon/amazon/spiders/amazon_spider.py
+++ amazon/amazon/spiders/amazon_spider.py
@@ -12,8 +12,11 @@ class AmazonSpider(CrawlSpider):
     start_urls = [
         "https://www.amazon.com/gp/product/B00XMVRVJM/ref=s9_simh_gw_g200_i2_r?ie=UTF8&fpl=fresh&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=10TVW2TVJTX0D37XKQ78&pf_rd_t=36701&pf_rd_p=6aad23bd-3035-4a40-b691-0eefb1a18396&pf_rd_i=desktop"
     ]
+    start_url_category = "Sports & Outdoors > Sports & Fitness > Hunting & Fishing > Tactical & Personal Defense > Tactical Knives"
+    already_crawled = []
+    
     rules = (
-        Rule(LxmlLinkExtractor(allow=(r'/dp',)), callback="parse_item", follow= True),
+        Rule(LxmlLinkExtractor(allow=(r'/dp',),deny=(r'/Detail/')), callback="parse_item", follow= True),
     )
 #start_urls = [
 	#"https://www.amazon.com/Mtech-Ballistic-Assisted-Tactical-Flipper/dp/B00RBF8EIC/ref=pd_sim_200_1?ie=UTF8&dpID=61mHW6oADBL&dpSrc=sims&preST=_AC_UL160_SR160%2C160_&psc=1&refRID=WFN5PJXS9PVQ836CX47G"
@@ -21,6 +24,18 @@ class AmazonSpider(CrawlSpider):
     
     def parse_item(self, response):
         item = AmazonItem()
+        str = ""
+        list_str = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()
+        i = 0
+        item['amazon_category'] = str.join(list_str) # Try this with Lambda and join.
+        for st in list_str:
+            str += st.strip(' \t\n\r')
+            i = i + 1
+            if(len(list_str) != i):
+                str += " > "
+        if str != AmazonSpider.start_url_category: 
+            return
+        item['amazon_category'] = str
         str = response.xpath('//span[@id="productTitle"]/text()').extract() # Working
         item['product_name'] = str[0].strip(' \t\n\r')
         str = response.xpath('//a[@id="brand"]/text()').extract()           # Working
@@ -52,6 +67,10 @@ class AmazonSpider(CrawlSpider):
                 val = cont.select('text()').extract()
                 val = re.sub(r'\W','',val[0]).strip(' \t\n\r')
                 item['item_ASIN'] = val
+            if str == 'Product Dimensions:':
+                val = cont.select('text()').extract()
+                val = re.sub(r'\W','',val[0]).strip(' \t\n\r')
+                item['product_dim'] = val
         #----------------------------------------------------------------------------#
         str = ""
         contents = ""
@@ -71,7 +90,6 @@ class AmazonSpider(CrawlSpider):
                 val = ""
                 list_str = cont.select('.//td/span/span/text()').extract_first()
                 val = val.join(list_str).strip(' \t\n\r()')
-                print (list_str)
                 item['item_rank'] = val
             if str == 'Item model number':
                 val = cont.select('.//td/text()').extract()
@@ -98,15 +116,7 @@ class AmazonSpider(CrawlSpider):
         #item['item_rank'] = str
         #---------------------------------------------------------------------------#
         str = ""
-        list_str = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()
-        i = 0
-        item['amazon_category'] = str.join(list_str) # Try this with Lambda and join.
-        for st in list_str:
-            str += st.strip(' \t\n\r')
-            i = i + 1
-            if(len(list_str) != i):
-                str += " > "           
-        item['amazon_category'] = str
+        
         #--------------------------------------------------------------------------------#
         str = response.xpath('//*[@id="reviewStarsLinkedCustomerReviews"]/i/span/text()').extract() #Not working
         str = str[0].strip(' \t\n\r')
